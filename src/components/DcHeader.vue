591e9d1575d17bf0f1f4be4805774290bee7fdbf
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-03 18:09:28 +0200

    Fourth step - array completed

--- src/components/DcHeader.vue
+++ src/components/DcHeader.vue
@@ -1,9 +1,76 @@
 <script>
 export default {
-    name: "DcHeader"
+    name: "DcHeader",
+    data() {
+        return {
+            linkings: [
+                {
+                    name: 'characters',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'comics',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'movies',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'tv',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'games',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'collectibles',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'videos',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'fans',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'news',
+                    url: '/',
+                    current: 'false'
+                },
+                {
+                    name: 'shop',
+                    url: '/',
+                    current: 'false'
+                }
+            ]
+        }
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <img src="../../public/img/dc-logo.png" alt="">
+    </header>
+    <nav>
+        <ul>
+            <li v-for="(element, index) in linkings" :key="index">
+                <a :class="element.current ? 'active' : '' '" :href="element.url">{{ element.name }}</a>
+            </li>
+        </ul>
+    </nav>
+</template>
 
 <style></style>
